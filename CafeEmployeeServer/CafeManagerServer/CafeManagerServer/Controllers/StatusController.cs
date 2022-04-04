f0c60a51a78468a2cf7e2e11bdf424cf164f106f
Shan Peiris <spe@dipscloud.com>
2022-04-04 12:59:26 +0530

    Adding helath endpoint

---
+++ CafeEmployeeServer/CafeManagerServer/CafeManagerServer/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CafeManagerServer.Controllers
+{
+    [ApiController]
+    [Route("api/status")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        [Route("health")]
+        public ActionResult GetCafeList(string cafeId)
+        {
+            try
+            {
+                return Ok("healthy server");
+            }
+            catch (Exception e)
+            {               
+                throw;
+            }
+
+        }
+    }
+}
